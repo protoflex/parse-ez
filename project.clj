48f8b89883906d4cfa57cad4339f1a1cf8b2365e
Panduranga Adusumilli <panad@protoflex.com>
2012-11-26 11:52:29 +0530

    version upgrade to 0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject parse-ez "0.3.1"
+(defproject parse-ez "0.3.2"
   :description "Clojure Parser Library"
   :url "https://github.com/protoflex/parse-ez"
   :license {:name "Eclipse Public License"}
